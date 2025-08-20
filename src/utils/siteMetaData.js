aab43f60a8b8ad670fbec871ecb472bce559d469
kaustavd7 <97113089+kaustavd7@users.noreply.github.com>
2025-08-21 00:39:01 +0530

    Update siteMetaData.js

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,12 +1,12 @@
 // please update required information
 const siteMetadata = {
-    title: 'Ajmal',
+    title: 'Kaayam',
     author: 'kaustav',
     headerTitle: 'Blog',
     description: 'essays and blogs from a 21 year old',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://kaayam.vercel.app', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
     email: 'kaustavvd21@gmail.com', 
