fd6f6b7c762c7cd1cf7082f8da1cad6c77944a5c
kaustavd7 <97113089+kaustavd7@users.noreply.github.com>
2025-08-21 16:55:28 +0530

    Update Skills.js

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -16,12 +16,17 @@ const Skills = () => {
       {/* Normal text paragraphs after bio */}
       <div className="mt-8 space-y-4">
         <p className="text-base sm:text-lg leading-relaxed">
-         I started writting about random stuff post summer-2024 to improve my articulations, under the umbrella of 'the placement prep'. I have penned down (not literally, i write in .doc) exactly 54 times, totalling word count of 24613 as of 21st August, 2025. Quite a few times i have resolved to write daily, clearly i have failed. Tho much of content can't be shared in open internet, but some of it can. So, after college ended, with ample amount of FREE time. I decided to create this website, but each time i tried, the code failed at the same spot. <br /> My mechanical degree and chatgpt gave up. 
-        <br /> Halfway through august, with few days in hand before i leave home on train to Hazrat Nizamuddin Station for my job, my brain had me telling this simple task had to be completed. "If not now, then forget it". So, this time taking the simpler path, i got the website up and running in few days with git-repos, chat-gpt and vercel. 
+         I started writting about random stuff post summer-twenty twenty four to improve my articulations, under the umbrella of 'the placement prep'. I have penned down (not literally, i write in .doc) exactly 54 times, totalling word count of 24613 as of twenty-first August, 2025. Quite a few times i have resolved to write daily, clearly i have failed. Tho much of content can't be shared in open internet, but some of it can. So, after college ended, with ample amount of FREE time. I decided to create this website, but each time i tried, the code failed at the same spot. <br /> My mechanical degree and chatgpt gave up. 
+        <br /> Halfway through august, with few days in hand before i leave home on train to Hazrat Nizamuddin Station for my job, my brain had me telling this simple task had to be completed. So, this time taking the simpler path, i got the website up and running in few days with git-repos, chat-gpt and vercel. 
         </p>
         <p className="text-base sm:text-lg leading-relaxed">
-          Now, the website the like a new toy for seven year old, to play with. You may find random component(not needed to the website), quotes, photos, with no correlations what-so-ever. I intend to keep it as my source of timepass and hence no advertisement as of yet.
+          Now, the website the like a new toy for seven year old, to play with. You may find random component(not needed to the website), quotes, photos, with no correlation or pattern what-so-ever. I intend to keep it as my source of timepass and hence no advertisement as of yet.
         </p>
+        <p className="text-base sm:text-lg leading-relaxed">
+          The name <i>kaayam</i>(qaayam) means ठहरना, ठहराव। arabic/persian word. <br /> I found the word when once i texted my friend "dunia kaayam h ishq ke wajah se, kuch toh sachai hogi isme". My curious mind wanted to know the literal meaning of the word - my heart loved the meaning, then with extension the word.
+        </p>
+        <p className="text-base sm:text-lg leading-relaxed">
+          The icon is collage of three frames from a fairly popular and good movie <i>la la land</i>. Liking a rom-com is actually extremely subjective. When you watch a WW2 film, everyone is fairly at same level, because the majority has never actually seen war. But rom-com - well, people have lived that stuff, so one is at the same level. Hence, a simple sweet rom-com is usually hit, as it resonates with masses. A complicated one, doesn't. la la land along with other elements had realistic ending, again resonating with mass. I like <i>materialist</i> for a very particular response in one of the dialougues. <br /> I personally don't like rom-com, full of cliche, predictable, normie stuff. But i like these two films. <br /> so yes romcom - subjective
       </div>
     
       <ul className="flex flex-wrap mt-8 justify-center  xs:justify-start">
