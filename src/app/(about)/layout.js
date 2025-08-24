8324ea70a55997d33cc966778ae72dfd34c96892
kaustavd7 <97113089+kaustavd7@users.noreply.github.com>
2025-08-24 17:04:22 +0530

    Update layout.js

--- src/app/(about)/layout.js
+++ src/app/(about)/layout.js
@@ -4,6 +4,15 @@ import InsightRoll from "@/src/components/About/InsightRoll";
 const insights = [
     "A Thousand Splendid Suns",
     "The Tattooist of Auschwitz",
+    "The Land I Dream Of",
+    "And The Mountains Echoced",
+    "Sea of Poppies",
+    "Forty Rules Of Love",
+    "The Kite Runner",
+    "The Book Thief",
+    "The Fountainhead",
+    "The Architect's Apprentice",
+    "Illusions"
   ];
 
 export default function AboutLayout({ children }) {
